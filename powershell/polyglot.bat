19ae229d68fb5a72e6646e8e927a0b1d6b935202
Daisuke Nakamura <nakamura.daisuke@gmail.com>
2020-08-15 15:51:12 +0900

    ---

--- powershell/polyglot.bat
+++ powershell/polyglot.bat
@@ -16,14 +16,3 @@ $VAR = "---- FROM POWERSHELL";
 $VAR;
 $POWERSHELL_BAT_ARGS=$env:POWERSHELL_BAT_ARGS
 $POWERSHELL_BAT_ARGS
-
-
-$taskPath = "\mytasks\";
-$taskName = "My backup task";
-$taskAction = New-ScheduledTaskAction -Execute "C:\home\zubora\batchan.m3u8" -WorkingDirectory "C:\home\zubora"
-$taskTrigger = New-ScheduledTaskTrigger -DaysInterval 1 -Daily -At "9:33"
-
-Register-ScheduledTask -TaskName $taskName `
-  -TaskPath $taskPath `
-  -Action $taskAction `
-  -Trigger $taskTrigger -Force
